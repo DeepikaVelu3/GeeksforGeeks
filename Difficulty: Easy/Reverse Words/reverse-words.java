75784ff11f35779e5d5a3656fabdfdbafd978d78
DeepikaVelu3 <deepikavelu003@gmail.com>
2026-03-27 17:33:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.java
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -1,19 +1,17 @@
 class Solution {
     public String reverseWords(String s) {
         // Code here
-        String[] str=s.split("\\.+");
-        StringBuilder sb=new StringBuilder();
-        for(int i=str.length-1;i>=0;i--){
-            if(str[i].length()==0){
-                continue;
-            }
-            if(sb.length()>0){
-                sb.append(".");
-            }
-            sb.append(str[i]);
-                
-            }
-        
-        return sb.toString();
+       String[] str = s.split("\\.+");
+       StringBuilder sb = new StringBuilder();
+       for(int i = str.length- 1;i>=0;i--){
+           if(str[i].length() == 0){
+               continue;
+           }
+           if(sb.length() > 0){
+               sb.append(".");
+           }
+           sb.append(str[i]);
+       }
+       return sb.toString();
     }
 }
