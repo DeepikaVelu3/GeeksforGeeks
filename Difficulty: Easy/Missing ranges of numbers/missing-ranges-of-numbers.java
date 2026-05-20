05eaf75c06475e430cc91c0fff3b3fda27fce8e8
DeepikaVelu3 <deepikavelu003@gmail.com>
2026-05-20 11:24:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing ranges of numbers/missing-ranges-of-numbers.java
+++ Difficulty: Easy/Missing ranges of numbers/missing-ranges-of-numbers.java
@@ -2,17 +2,20 @@ class Solution {
     public List<List<Integer>> missingRanges(int[] arr, int lower, int upper) {
         // code here
         List<List<Integer>> l=new ArrayList<>();
-        int prev=lower-1;
+        int prev=lower;
         
-        for(int i=0;i<arr.length;i++){
-            int curr=arr[i];
-            if(curr-prev>=2)
-            l.add(Arrays.asList(prev+1,curr-1));
-            prev=curr;
-           
+        for(int num : arr){
+            if(prev<num){
+                
+            l.add(Arrays.asList(prev,num-1));
+            }
+               prev=num+1;
+        
         }
-        if(upper-prev>=1)
-            l.add(Arrays.asList(prev+1,upper));
-        return l;
+     
+       if(prev <= upper){
+           l.add(Arrays.asList(prev,upper));
+       }
+       return l;
     }
 }
