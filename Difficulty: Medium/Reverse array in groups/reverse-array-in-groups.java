dc8980791789ba1445e1b32e823abd7ac1c6e8bc
DeepikaVelu3 <deepikavelu003@gmail.com>
2026-05-21 14:47:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Reverse array in groups/reverse-array-in-groups.java
+++ Difficulty: Medium/Reverse array in groups/reverse-array-in-groups.java
@@ -2,17 +2,16 @@ class Solution {
     public void reverseInGroups(int[] arr, int k) {
         // code here
         int n = arr.length;
-        for(int i=0;i<n;i+=k){
+        for(int i =0;i < n;i+=k){
         int left = i;
         int right = Math.min(n-1,i+k-1);
-        
-          while(left<right)  {
-              int temp = arr[left];
-              arr[left] = arr[right];
-              arr[right] = temp;
-              left++;
-              right--;
-          }
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
         }
     }
 }
