9cfd96709f3e0b4c9e0e55ff8d8d07862a5bd5ca
DeepikaVelu3 <deepikavelu003@gmail.com>
2026-06-19 11:36:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+     public int isPartioningCount(int[] arr ,int mid){
+        int partitioning = 1;
+        int sum = 0;
+        for(int i : arr){
+            if(sum + i <= mid){
+                sum+= i;
+            }
+            else{
+                partitioning++;
+                sum = i;
+                
+            }
+        }
+         return partitioning;
+    }
+    public int minTime(int[] arr, int k) {
+        // code here
+           int low = 0;
+        int high = 0;
+        for(int i  : arr){
+            low = Math.max(low,i);
+            high += i;
+        }
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(isPartioningCount(arr , mid) <=k){
+                high = mid-1;
+                
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return low;
+        
+    }
+}
